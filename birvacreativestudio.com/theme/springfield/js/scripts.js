ac50afa86f75e2d2a0399cf86a5fdc21af3f0003
hevelmo <hevelmo060683@gmail.com>
2016-02-24 17:04:13 -0600

    commit

--- birvacreativestudio.com/theme/springfield/js/scripts.js
+++ birvacreativestudio.com/theme/springfield/js/scripts.js
@@ -12,18 +12,18 @@ if(!Modernizr.touch){
 	$(document).ready(function(){
 		$(".navbar").sticky({topSpacing:0});
 	});
-	
+
 	/* ==============================================
 	Auto Close Responsive Navbar on Click
 	=============================================== */
 	close_toggle();
-	$(window).resize(close_toggle); 
+	$(window).resize(close_toggle);
 	$(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
-	
+
 	/* ==============================================
 	WOW plugin triggers animation.css on scroll
 	=============================================== */
-	
+
 	var wow = new WOW(
 	  {
 		boxClass:     'wow',      // animated element css class (default is wow)
@@ -33,12 +33,12 @@ if(!Modernizr.touch){
 	  }
 	);
 	wow.init();
-	
-	
+
+
 	/* ==============================================
 	Fit Vids
 	=============================================== */
-	
+
 	$(".fluid-width-video-wrapper").fitVids();
 
 	/* ==============================================
@@ -51,19 +51,19 @@ if(!Modernizr.touch){
 				width:jQuery(this).attr('data-percent')
 			},2000);
 		});
-		
-		}, { offset: '100%' 
+
+		}, { offset: '100%'
 	});
-	
+
 	/* ==============================================
 	Parallax
 	=============================================== */
-	$(window).stellar({ 
+	$(window).stellar({
           horizontalScrolling: false,
 		  verticalOffset: 40,
           responsive: true
-	  });  
-	  
+	  });
+
 	/* ==============================================
 	Home Slider
 	=============================================== */
@@ -78,7 +78,7 @@ if(!Modernizr.touch){
 		itemsTablet: [600,1], //2 items between 600 and 0
         navigationText: false
     });
-	
+
 	/* ==============================================
 	Quotes Slider
 	=============================================== */
@@ -93,7 +93,7 @@ if(!Modernizr.touch){
 		itemsTablet: [600,1], //2 items between 600 and 0
         navigationText: false
     });
-	
+
 	/* ==============================================
 	Testimonial Slider
 	=============================================== */
@@ -134,7 +134,7 @@ if(!Modernizr.touch){
 		ticker: true,
 		speed: 40000
 	});
-	
+
 	/* ==============================================
 	On resize Clients and Gallery Slider
 	=============================================== */
@@ -142,7 +142,7 @@ if(!Modernizr.touch){
 		clients_slider.reloadSlider();
 		gallery_slider.reloadSlider();
 	});
-	
+
 	/* ==============================================
 	Contact Form
 	=============================================== */
@@ -171,13 +171,13 @@ if(!Modernizr.touch){
 		});
 		return false;
 	});
-	
+
 	/* ==============================================
 	Google Maps
 	=============================================== */
 
 	Map.init();
-	
+
 	/* ==============================================
 	Back to Top
 	=============================================== */
@@ -188,12 +188,12 @@ if(!Modernizr.touch){
 			$("#back-to-top").fadeOut(600);
 		}
 	});
-	
+
 	$('#back-to-top, .back-to-top').click(function() {
 		  $('html, body').animate({ scrollTop:0 }, '1000');
 		  return false;
 	});
-	
+
 	/* ==============================================
 	Smooth scrolling to anchor for home buttons
 	=============================================== */
@@ -230,25 +230,25 @@ if(!Modernizr.touch){
 			event.preventDefault();
 		});
 	});
-		
+
 	/* ==============================================
 	Active Menu Item on Page Scroll
-	=============================================== */   
+	=============================================== */
 	var sections = $('section')
 	  , nav = $('nav')
 	  , nav_height = nav.outerHeight();
-	 
+
 	$(window).on('scroll', function () {
 	  var cur_pos = $(this).scrollTop();
-	 
+
 	  sections.each(function() {
 		var top = $(this).offset().top - nav_height,
 			bottom = top + $(this).outerHeight();
-	 
+
 		if (cur_pos >= top && cur_pos <= bottom) {
 		  nav.find('a').removeClass('current');
 		  sections.removeClass('current');
-	 
+
 		  $(this).addClass('current');
 		  nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('current');
 		}
@@ -298,7 +298,7 @@ if(!Modernizr.touch){
 			});
 	});
 
-	
+
 }(jQuery));
 
 
@@ -318,7 +318,7 @@ function close_toggle() {
 
 
 
-    
+
 
 
 
